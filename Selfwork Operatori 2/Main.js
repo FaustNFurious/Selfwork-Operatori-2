2dec18357e8bd6ba40b8bf59ce71da25658013b8
FaustNFurious <lorelego2003@gmail.com>
2024-11-11 21:13:23 +0100

    Add files via upload
    Svolto esercizio operatori 2

---
+++ Selfwork Operatori 2/Main.js
@@ -0,0 +1,11 @@
+// Dichiarazione Variabili
+let annoCorrente = 2024;
+let dataNascita = 2003;
+let secolo = 100;
+
+// Calcolo età e per arrivare a 100 anni
+let età = annoCorrente - dataNascita;
+let anniMancanti = secolo - età;
+
+// Stampa a schermo degli anni 
+console.log(`Hai ${età} anni e ti mancano ${anniMancanti} anni per compierne 100`);
